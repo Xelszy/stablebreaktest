d01837ef4b817594f27f98b125432eb7958a6ca6
Deo.SiriliusVansaHavian <48593365+Xelszy@users.noreply.github.com>
2023-05-30 17:09:14 +0700

    Update stablebreaktest.py

--- stablebreaktest.py
+++ stablebreaktest.py
@@ -3,7 +3,7 @@ import subprocess
 repo_path = '/content/stablebreaktest'
 
 codetorun = """
-!git clone -b v2.1 https://github.com/camenduru/stable-diffusion-webui /content/stablebreaktest
+!git clone https://github.com/NoCrypt/stable-diffusion-webui /content/stablebreaktest
 !git clone https://github.com/Xelszy/sd-webui-additional-networks /content/stablebreaktest/extensions/sd-webui-additional-networks
 !git checkout e9f3d62
 !git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git /content/stablebreaktest/extensions/tag-autocomplete
