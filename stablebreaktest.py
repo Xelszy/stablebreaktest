6d7be6caaf24b00354a86efd73f2561225936e55
Deo.SiriliusVansaHavian <48593365+Xelszy@users.noreply.github.com>
2023-06-01 18:12:00 +0700

    Update stablebreaktest.py

--- stablebreaktest.py
+++ stablebreaktest.py
@@ -17,11 +17,7 @@ codetorun = """
 !git clone https://github.com/zanllp/sd-webui-infinite-image-browsing /content/stablebreaktest/extensions/sd-webui-infinite-image-browsing 
 !git clone https://github.com/takoyaro/db-storage1111 /content/stablebreaktest/extensions/db-storage1111
 !git clone https://github.com/arenatemp/stable-diffusion-webui-model-toolkit /content/stablebreaktest/extensions/stable-diffusion-webui-model-toolkit
-!git clone https://github.com/KohakuBlueleaf/a1111-sd-webui-locon /content/stablebreaktest/extensions/a1111-sd-webui-locon
-
-%cd /content/stablebreaktest/extensions/a1111-sd-webui-locon
-!git checkout 04b768b
-%cd /content/stablebreaktest
+!git clone https://github.com/KohakuBlueleaf/a1111-sd-webui-lycoris /content/stablebreaktest/extensions/a1111-sd-webui-lycoris
 
 !git clone https://github.com/camenduru/sd-civitai-browser /content/stablebreaktest/extensions/sd-civitai-browser
 !git clone https://github.com/Mikubill/sd-webui-controlnet /content/stablebreaktest/extensions/sd-webui-controlnet
