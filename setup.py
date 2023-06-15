914addde6c6bf56c2a0f12425041ceb97386a5eb
Deo.SiriliusVansaHavian <48593365+Xelszy@users.noreply.github.com>
2023-06-15 10:12:29 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='colablib',
+    version='0.1.8',
+    packages=find_packages(),
+    install_requires=[
+        'safetensors==0.2.6',
+        'requests==2.27.1',
+        'tqdm==4.65.0',
+        'PyYAML==6.0',
+        'gdown==4.7.1',
+        'toml==0.10.2',
+        'rarfile==4.0',
+        'xmltodict==0.13.0',
+        'mega.py==1.0.8',
+        'pydantic==1.10.8'
+    ],
+    url='https://github.com/Linaqruf/colablib',
+)
